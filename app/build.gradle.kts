6361721641f895c09df8b9032953fc3759005e47
matias.norambuena <matias.norambuena@neolab.cl>
2024-03-30 19:59:45 -0300

    se agrega inicio de sesión, recuperación de contraseña y terminos y condiciones

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
 
     defaultConfig {
         applicationId = "com.matiasdev.wildrunning"
-        minSdk = 21
+        minSdk = 24
         targetSdk = 33
         versionCode = 1
         versionName = "1.0"
@@ -54,7 +54,7 @@ dependencies {
     implementation("com.google.firebase:firebase-database:20.3.1")
     implementation("com.google.firebase:firebase-firestore")
     implementation(platform("com.google.firebase:firebase-bom:32.7.4"))
-    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
@@ -65,6 +65,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.firebase:firebase-auth-ktx:22.3.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/matiasdev/wildrunning/LoginActivity.kt
+++ app/src/main/java/com/matiasdev/wildrunning/LoginActivity.kt
@@ -1,11 +1,131 @@
 package com.matiasdev.wildrunning
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.view.View
+import android.widget.CheckBox
+import android.widget.EditText
+import android.widget.LinearLayout
+import android.widget.Toast
+import androidx.compose.ui.unit.TextUnit
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import java.text.SimpleDateFormat
+import java.util.Date
+import kotlin.properties.Delegates
 
 class LoginActivity : AppCompatActivity() {
+
+    companion object {
+        lateinit var useremail: String
+        lateinit var providerSession: String
+    }
+
+    private var email by Delegates.notNull<String>()
+    private var password by Delegates.notNull<String>()
+    private lateinit var etEmail: EditText
+    private lateinit var etPassword: EditText
+    private lateinit var lyTerms: LinearLayout
+
+    private lateinit var mAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        lyTerms = findViewById(R.id.lyTerms)
+        lyTerms.visibility = View.INVISIBLE
+
+        etEmail = findViewById(R.id.etEmail)
+        etPassword = findViewById(R.id.etPassword)
+        mAuth = FirebaseAuth.getInstance()
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        val currentUser = FirebaseAuth.getInstance().currentUser
+        if (currentUser != null) goHome(currentUser.email.toString(),currentUser.providerId)
+    }
+
+    @SuppressLint("MissingSuperCall")
+    override fun onBackPressed() {
+        val startMain = Intent(Intent.ACTION_MAIN)
+        startMain.addCategory(Intent.CATEGORY_HOME)
+        startMain.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+        startActivity(startMain)
+    }
+
+    fun login(view: View) {
+        loginUser()
+    }
+
+    private fun loginUser() {
+        email = etEmail.text.toString()
+        password = etPassword.text.toString()
+
+        mAuth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) goHome(email, "email")
+                else {
+                    if (lyTerms.visibility == View.INVISIBLE) lyTerms.visibility = View.VISIBLE
+                    else {
+                        var cbAccept = findViewById<CheckBox>(R.id.cbAccept)
+                        if (cbAccept.isChecked) register()
+                    }
+                }
+            }
+    }
+
+    private fun goHome(email: String, provider: String) {
+        useremail = email
+        providerSession = provider
+
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun register() {
+        email = etEmail.text.toString()
+        password = etPassword.text.toString()
+
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener {
+                if (it.isSuccessful) {
+                    print("creada")
+                    var dateRegister = SimpleDateFormat("dd/MM/yyyy").format(Date())
+                    var dbRegister = FirebaseFirestore.getInstance()
+                    dbRegister.collection("users").document(email).set(hashMapOf(
+                        "user" to email,
+                        "dateRegister" to dateRegister
+                    ))
+
+                    goHome(email, "email")
+                } else Toast.makeText(this, "Error, algo ha ido mal : (", Toast.LENGTH_SHORT).show()
+            }
+    }
+
+    fun goTerms(v: View){
+        val intent = Intent(this, TermsActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun forgotPassword(v: View){
+        resetPassword()
+    }
+
+    private fun resetPassword(){
+        val e = etEmail.text.toString()
+        if (!TextUtils.isEmpty(e)){
+            mAuth.sendPasswordResetEmail(e)
+                .addOnCompleteListener{
+                    task ->
+                    if (task.isSuccessful) Toast.makeText(this, "Email Enviado a $e", Toast.LENGTH_SHORT).show()
+                    else Toast.makeText(this, "No se encontró el usuario con este correo", Toast.LENGTH_SHORT).show()
+                }
+        }
+        else Toast.makeText(this, "Indica un email", Toast.LENGTH_SHORT).show()
     }
 }
