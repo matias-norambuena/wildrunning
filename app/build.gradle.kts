d5fc9faaffb31c3a1bff182b3119b6704187f9b7
matias.norambuena <matias.norambuena@neolab.cl>
2024-04-28 00:59:16 -0400

    se agrega toolbar

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,6 +67,7 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.google.firebase:firebase-auth-ktx:22.3.1")
     implementation("com.google.android.gms:play-services-auth:21.0.0")
+    implementation("me.tankery.lib:circularSeekBar:1.3.2")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/matiasdev/wildrunning/MainActivity.kt
+++ app/src/main/java/com/matiasdev/wildrunning/MainActivity.kt
@@ -2,26 +2,65 @@ package com.matiasdev.wildrunning
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import android.widget.TextView
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 import com.google.firebase.auth.FirebaseAuth
+import com.matiasdev.wildrunning.LoginActivity.Companion.useremail
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    private lateinit var drawer: DrawerLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main)
+
+        initToolBar()
+        initNavigationView()
     }
 
-    fun callSignOff(view: View){
+    private fun initToolBar() {
+        val toolbar: Toolbar = findViewById(R.id.toolbar_main)
+        setSupportActionBar(toolbar)
+
+        drawer = findViewById(R.id.drawer_layout)
+        val toggle = ActionBarDrawerToggle(
+            this, drawer, toolbar, R.string.bar_title, R.string.navigation_drawer_close
+        )
+
+        drawer.addDrawerListener(toggle)
+        toggle.syncState()
+
+    }
+
+    private fun initNavigationView() {
+        var navigationView: NavigationView = findViewById(R.id.nav_view)
+        navigationView.setNavigationItemSelectedListener(this)
+        var headerView: View =
+            LayoutInflater.from(this).inflate(R.layout.nav_header_main, navigationView, false)
+        navigationView.removeHeaderView(headerView)
+        navigationView.addHeaderView(headerView)
+
+        var tvUser: TextView = headerView.findViewById(R.id.tvUser)
+        tvUser.text = useremail
+    }
+
+    fun callSignOff(view: View) {
         signOff()
     }
-    private fun signOff(){
+
+    private fun signOff() {
         FirebaseAuth.getInstance().signOut()
         startActivity(Intent(this, LoginActivity::class.java))
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        TODO("Not yet implemented")
     }
 }
