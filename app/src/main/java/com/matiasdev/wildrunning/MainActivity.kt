877adc0a642f3474acc2817b745ee3df2ecdba3a
matias.norambuena <matias.norambuena@neolab.cl>
2024-05-02 00:31:49 -0400

    se agrega control de number picker

--- app/src/main/java/com/matiasdev/wildrunning/MainActivity.kt
+++ app/src/main/java/com/matiasdev/wildrunning/MainActivity.kt
@@ -9,6 +9,7 @@ import android.view.LayoutInflater
 import android.view.MenuItem
 import android.view.View
 import android.widget.LinearLayout
+import android.widget.NumberPicker
 import android.widget.Switch
 import android.widget.TextView
 import androidx.appcompat.app.ActionBarDrawerToggle
@@ -16,6 +17,7 @@ import androidx.appcompat.widget.Toolbar
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat
 import androidx.core.view.GravityCompat
+import androidx.core.view.isVisible
 import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.navigation.NavigationView
 import com.google.api.Distribution.BucketOptions.Linear
@@ -23,18 +25,78 @@ import com.google.firebase.auth.FirebaseAuth
 import com.matiasdev.wildrunning.LoginActivity.Companion.useremail
 import com.matiasdev.wildrunning.Utility.animateViewofFloat
 import com.matiasdev.wildrunning.Utility.animateViewofInt
+import com.matiasdev.wildrunning.Utility.getFormattedStopWatch
+import com.matiasdev.wildrunning.Utility.getSecFromWatch
 import com.matiasdev.wildrunning.Utility.setHeightLinearLayout
+import me.tankery.lib.circularseekbar.CircularSeekBar
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
+    private var widthScreenPixels: Int = 0
+    private var heightScreenPixels: Int = 0
+    private var widthAnimations: Int = 0
+
     private lateinit var drawer: DrawerLayout
+
+    private lateinit var csbChallengeDistance: CircularSeekBar
+    private lateinit var csbCurrentDistance: CircularSeekBar
+    private lateinit var csbRecordDistance: CircularSeekBar
+
+    private lateinit var csbCurrentAvgSpeed: CircularSeekBar
+    private lateinit var csbRecordAvgSpeed: CircularSeekBar
+
+    private lateinit var csbCurrentSpeed: CircularSeekBar
+    private lateinit var csbCurrentMaxSpeed: CircularSeekBar
+    private lateinit var csbRecordSpeed: CircularSeekBar
+
+    private lateinit var tvDistanceRecord: TextView
+    private lateinit var tvAvgSpeedRecord: TextView
+    private lateinit var tvMaxSpeedRecord: TextView
+
 
     private lateinit var swIntervalMode: Switch
     private lateinit var swChallenges: Switch
     private lateinit var swVolumes: Switch
 
+    private lateinit var npChallengeDistance: NumberPicker
+    private lateinit var npChallengeDurationHH: NumberPicker
+    private lateinit var npChallengeDurationMM: NumberPicker
+    private lateinit var npChallengeDurationSS: NumberPicker
+
     private var challengeDistance: Float = 0f
     private var challengeDuration: Int = 0
+
+    private lateinit var tvChrono: TextView
+
+    private lateinit var npDurationInterval: NumberPicker
+    private lateinit var tvRunningTime: TextView
+    private lateinit var tvWalkingTime: TextView
+    private lateinit var csbRunWalk: CircularSeekBar
+
+    private var ROUND_INTERVAL = 300
+    private var TIME_RUNNING: Int = 0
+
+    private lateinit var lyPopupRun: LinearLayout
+
+    private fun initStopWatch() {
+        tvChrono.text = getString(R.string.init_stop_watch_value)
+    }
+    private fun initChrono(){
+        tvChrono = findViewById(R.id.tvChrono)
+        tvChrono.setTextColor(ContextCompat.getColor( this, R.color.white))
+        initStopWatch()
+
+        widthScreenPixels = resources.displayMetrics.widthPixels
+        heightScreenPixels = resources.displayMetrics.heightPixels
+
+        widthAnimations = widthScreenPixels
+
+
+        val lyChronoProgressBg = findViewById<LinearLayout>(R.id.lyChronoProgressBg)
+        val lyRoundProgressBg = findViewById<LinearLayout>(R.id.lyRoundProgressBg)
+        lyChronoProgressBg.translationX = -widthAnimations.toFloat()
+        lyRoundProgressBg.translationX = -widthAnimations.toFloat()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,6 +105,290 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         initToolBar()
         initObjects()
         initNavigationView()
+    }
+
+    private fun hideLayouts(){
+        var lyMap = findViewById<LinearLayout>(R.id.lyMap)
+        var lyFragmentMap = findViewById<LinearLayout>(R.id.lyFragmentMap)
+        val lyIntervalModeSpace = findViewById<LinearLayout>(R.id.lyIntervalModeSpace)
+        val lyIntervalMode = findViewById<LinearLayout>(R.id.lyIntervalMode)
+        val lyChallengesSpace = findViewById<LinearLayout>(R.id.lyChallengesSpace)
+        val lyChallenges = findViewById<LinearLayout>(R.id.lyChallenges)
+        val lySettingsVolumesSpace = findViewById<LinearLayout>(R.id.lySettingsVolumesSpace)
+        val lySettingsVolumes = findViewById<LinearLayout>(R.id.lySettingsVolumes)
+        var lySoftTrack = findViewById<LinearLayout>(R.id.lySoftTrack)
+        var lySoftVolume = findViewById<LinearLayout>(R.id.lySoftVolume)
+
+
+        setHeightLinearLayout(lyMap, 0)
+        setHeightLinearLayout(lyIntervalModeSpace,0)
+        setHeightLinearLayout(lyChallengesSpace,0)
+        setHeightLinearLayout(lySettingsVolumesSpace,0)
+        setHeightLinearLayout(lySoftTrack,0)
+        setHeightLinearLayout(lySoftVolume,0)
+
+        lyFragmentMap.translationY = -300f
+        lyIntervalMode.translationY = -300f
+        lyChallenges.translationY = -300f
+        lySettingsVolumes.translationY = -300f
+    }
+
+    private fun initSwitchs(){
+        swIntervalMode = findViewById(R.id.swIntervalMode)
+        swChallenges = findViewById(R.id.swChallenges)
+        swVolumes = findViewById(R.id.swVolumes)
+    }
+    private fun initIntervalMode(){
+        npDurationInterval = findViewById(R.id.npDurationInterval)
+        tvRunningTime = findViewById(R.id.tvRunningTime)
+        tvWalkingTime = findViewById(R.id.tvWalkingTime)
+        csbRunWalk = findViewById(R.id.csbRunWalk)
+
+        npDurationInterval.minValue = 1
+        npDurationInterval.maxValue = 60
+        npDurationInterval.value = 5
+        npDurationInterval.wrapSelectorWheel = true
+        npDurationInterval.setFormatter(NumberPicker.Formatter { i -> String.format("%02d", i) })
+
+        npDurationInterval.setOnValueChangedListener { picker, oldVal, newVal ->
+            csbRunWalk.max = (newVal*60).toFloat()
+            csbRunWalk.progress = csbRunWalk.max/2
+
+
+            tvRunningTime.text = getFormattedStopWatch(((newVal*60/2)*1000).toLong()).subSequence(3,8)
+            tvWalkingTime.text = tvRunningTime.text
+
+            ROUND_INTERVAL = newVal * 60
+            TIME_RUNNING = ROUND_INTERVAL / 2
+        }
+
+        csbRunWalk.max = 300f
+        csbRunWalk.progress = 150f
+        csbRunWalk.setOnSeekBarChangeListener(object :
+            CircularSeekBar.OnCircularSeekBarChangeListener {
+            override fun onProgressChanged(circularSeekBar: CircularSeekBar,progress: Float,fromUser: Boolean) {
+
+                if (fromUser){
+                    var STEPS_UX: Int = 15
+                    if (ROUND_INTERVAL > 600) STEPS_UX = 60
+                    if (ROUND_INTERVAL > 1800) STEPS_UX = 300
+                    var set: Int = 0
+                    var p = progress.toInt()
+
+                    var limit = 60
+                    if (ROUND_INTERVAL > 1800) limit = 300
+
+                    if (p%STEPS_UX != 0 && progress != csbRunWalk.max){
+                        while (p >= limit) p -= limit
+                        while (p >= STEPS_UX) p -= STEPS_UX
+                        if (STEPS_UX-p > STEPS_UX/2) set = -1 * p
+                        else set = STEPS_UX-p
+
+                        if (csbRunWalk.progress + set > csbRunWalk.max)
+                            csbRunWalk.progress = csbRunWalk.max
+                        else
+                            csbRunWalk.progress = csbRunWalk.progress + set
+                    }
+                }
+
+                tvRunningTime.text = getFormattedStopWatch((csbRunWalk.progress.toInt() *1000).toLong()).subSequence(3,8)
+                tvWalkingTime.text = getFormattedStopWatch(((ROUND_INTERVAL- csbRunWalk.progress.toInt())*1000).toLong()).subSequence(3,8)
+                TIME_RUNNING = getSecFromWatch(tvRunningTime.text.toString())
+            }
+
+            override fun onStopTrackingTouch(seekBar: CircularSeekBar) {
+            }
+
+            override fun onStartTrackingTouch(seekBar: CircularSeekBar) {
+            }
+        })
+    }
+
+    fun inflateIntervalMode(v: View){
+        val lyIntervalMode = findViewById<LinearLayout>(R.id.lyIntervalMode)
+        val lyIntervalModeSpace = findViewById<LinearLayout>(R.id.lyIntervalModeSpace)
+        var lySoftTrack = findViewById<LinearLayout>(R.id.lySoftTrack)
+        var lySoftVolume = findViewById<LinearLayout>(R.id.lySoftVolume)
+        var tvRounds = findViewById<TextView>(R.id.tvRounds)
+
+        if (swIntervalMode.isChecked){
+            animateViewofInt(swIntervalMode, "textColor", ContextCompat.getColor(this, R.color.orange), 500)
+            setHeightLinearLayout(lyIntervalModeSpace, 600)
+            animateViewofFloat(lyIntervalMode, "translationY", 0f, 500)
+            animateViewofFloat (tvChrono, "translationX", -110f, 500)
+            tvRounds.setText(R.string.rounds)
+            animateViewofInt(tvRounds, "textColor", ContextCompat.getColor(this, R.color.white), 500)
+
+            setHeightLinearLayout(lySoftTrack,120)
+            setHeightLinearLayout(lySoftVolume,200)
+            if (swVolumes.isChecked){
+                var lySettingsVolumesSpace = findViewById<LinearLayout>(R.id.lySettingsVolumesSpace)
+                setHeightLinearLayout(lySettingsVolumesSpace,600)
+            }
+        }
+        else{
+            swIntervalMode.setTextColor(ContextCompat.getColor(this, R.color.white))
+            setHeightLinearLayout(lyIntervalModeSpace,0)
+            lyIntervalMode.translationY = -200f
+            animateViewofFloat (tvChrono, "translationX", 0f, 500)
+            tvRounds.text = ""
+            setHeightLinearLayout(lySoftTrack,0)
+            setHeightLinearLayout(lySoftVolume,0)
+            if (swVolumes.isChecked){
+                var lySettingsVolumesSpace = findViewById<LinearLayout>(R.id.lySettingsVolumesSpace)
+                setHeightLinearLayout(lySettingsVolumesSpace,400)
+            }
+        }
+    }
+
+    fun showDuration(v: View){
+        showChallenge("duration")
+    }
+    fun showDistance(v:View){
+        showChallenge("distance")
+    }
+    private fun showChallenge(option: String){
+        var lyChallengeDuration = findViewById<LinearLayout>(R.id.lyChallengeDuration)
+        var lyChallengeDistance = findViewById<LinearLayout>(R.id.lyChallengeDistance)
+        var tvChallengeDuration = findViewById<TextView>(R.id.tvChallengeDuration)
+        var tvChallengeDistance = findViewById<TextView>(R.id.tvChallengeDistance)
+
+        when (option){
+            "duration" ->{
+                lyChallengeDuration.translationZ = 5f
+                lyChallengeDistance.translationZ = 0f
+
+                tvChallengeDuration.setTextColor(ContextCompat.getColor(this, R.color.orange))
+                tvChallengeDuration.setBackgroundColor(ContextCompat.getColor(this, R.color.gray_dark))
+
+                tvChallengeDistance.setTextColor(ContextCompat.getColor(this, R.color.white))
+                tvChallengeDistance.setBackgroundColor(ContextCompat.getColor(this, R.color.gray_medium))
+
+                challengeDistance = 0f
+                getChallengeDuration(npChallengeDurationHH.value, npChallengeDurationMM.value, npChallengeDurationSS.value)
+            }
+            "distance" -> {
+                lyChallengeDuration.translationZ = 0f
+                lyChallengeDistance.translationZ = 5f
+
+                tvChallengeDuration.setTextColor(ContextCompat.getColor(this, R.color.white))
+                tvChallengeDuration.setBackgroundColor(ContextCompat.getColor(this, R.color.gray_medium))
+
+                tvChallengeDistance.setTextColor(ContextCompat.getColor(this, R.color.orange))
+                tvChallengeDistance.setBackgroundColor(ContextCompat.getColor(this, R.color.gray_dark))
+
+                challengeDuration = 0
+                challengeDistance = npChallengeDistance.value.toFloat()
+            }
+        }
+    }
+
+    private fun getChallengeDuration(hh: Int, mm: Int, ss: Int){
+        var hours: String = hh.toString()
+        if (hh<10) hours = "0"+hours
+        var minutes: String = mm.toString()
+        if (mm<10) minutes = "0"+minutes
+        var seconds: String = ss.toString()
+        if (ss<10) seconds = "0"+seconds
+
+        challengeDuration = getSecFromWatch("${hours}:${minutes}:${seconds}")
+    }
+
+    private fun initChallengeMode(){
+        npChallengeDistance = findViewById(R.id.npChallengeDistance)
+        npChallengeDurationHH = findViewById(R.id.npChallengeDurationHH)
+        npChallengeDurationMM = findViewById(R.id.npChallengeDurationMM)
+        npChallengeDurationSS = findViewById(R.id.npChallengeDurationSS)
+
+        npChallengeDistance.minValue = 1
+        npChallengeDistance.maxValue = 300
+        npChallengeDistance.value = 10
+        npChallengeDistance.wrapSelectorWheel = true
+
+
+        npChallengeDistance.setOnValueChangedListener { picker, oldVal, newVal ->
+            challengeDistance = newVal.toFloat()
+            csbChallengeDistance.max = newVal.toFloat()
+            csbChallengeDistance.progress = newVal.toFloat()
+            challengeDuration = 0
+
+            if (csbChallengeDistance.max > csbRecordDistance.max)
+                csbCurrentDistance.max = csbChallengeDistance.max
+        }
+
+        npChallengeDurationHH.minValue = 0
+        npChallengeDurationHH.maxValue = 23
+        npChallengeDurationHH.value = 1
+        npChallengeDurationHH.wrapSelectorWheel = true
+        npChallengeDurationHH.setFormatter(NumberPicker.Formatter { i -> String.format("%02d", i) })
+
+        npChallengeDurationMM.minValue = 0
+        npChallengeDurationMM.maxValue = 59
+        npChallengeDurationMM.value = 0
+        npChallengeDurationMM.wrapSelectorWheel = true
+        npChallengeDurationMM.setFormatter(NumberPicker.Formatter { i -> String.format("%02d", i) })
+
+        npChallengeDurationSS.minValue = 0
+        npChallengeDurationSS.maxValue = 59
+        npChallengeDurationSS.value = 0
+        npChallengeDurationSS.wrapSelectorWheel = true
+        npChallengeDurationSS.setFormatter(NumberPicker.Formatter { i -> String.format("%02d", i) })
+
+        npChallengeDurationHH.setOnValueChangedListener { picker, oldVal, newVal ->
+            getChallengeDuration(newVal, npChallengeDurationMM.value, npChallengeDurationSS.value)
+        }
+        npChallengeDurationMM.setOnValueChangedListener { picker, oldVal, newVal ->
+            getChallengeDuration(npChallengeDurationHH.value, newVal, npChallengeDurationSS.value)
+        }
+        npChallengeDurationSS.setOnValueChangedListener { picker, oldVal, newVal ->
+            getChallengeDuration(npChallengeDurationHH.value, npChallengeDurationMM.value, newVal)
+        }
+
+    }
+    private fun hidePopUpRun(){
+        var lyWindow = findViewById<LinearLayout>(R.id.lyWindow)
+        lyWindow.translationX = 400f
+        lyPopupRun = findViewById(R.id.lyPopupRun)
+        lyPopupRun.isVisible = false
+    }
+    private fun initObjects(){
+        initChrono()
+        hideLayouts()
+        initMetrics()
+        initSwitchs()
+        initIntervalMode()
+        initChallengeMode()
+        hidePopUpRun()
+    }
+
+
+    private fun initMetrics(){
+        csbCurrentDistance = findViewById(R.id.csbCurrentDistance)
+        csbChallengeDistance = findViewById(R.id.csbChallengeDistance)
+        csbRecordDistance = findViewById(R.id.csbRecordDistance)
+
+        csbCurrentAvgSpeed = findViewById(R.id.csbCurrentAvgSpeed)
+        csbRecordAvgSpeed = findViewById(R.id.csbRecordAvgSpeed)
+
+        csbCurrentSpeed = findViewById(R.id.csbCurrentSpeed)
+        csbCurrentMaxSpeed = findViewById(R.id.csbCurrentMaxSpeed)
+        csbRecordSpeed = findViewById(R.id.csbRecordSpeed)
+
+        csbCurrentDistance.progress = 0f
+        csbChallengeDistance.progress = 0f
+
+        csbCurrentAvgSpeed.progress = 0f
+
+        csbCurrentSpeed.progress = 0f
+        csbCurrentMaxSpeed.progress = 0f
+
+        tvDistanceRecord = findViewById(R.id.tvDistanceRecord)
+        tvAvgSpeedRecord = findViewById(R.id.tvAvgSpeedRecord)
+        tvMaxSpeedRecord = findViewById(R.id.tvMaxSpeedRecord)
+
+        tvDistanceRecord.text = ""
+        tvAvgSpeedRecord.text = ""
+        tvMaxSpeedRecord.text = ""
     }
 
     @SuppressLint("MissingSuperCall")
@@ -64,35 +410,6 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         drawer.addDrawerListener(toggle)
         toggle.syncState()
 
-    }
-    private fun initObjects(){
-
-        var lyMap = findViewById<LinearLayout>(R.id.lyMap)
-        var lyFragmentMap = findViewById<LinearLayout>(R.id.lyFragmentMap)
-        val lyIntervalModeSpace = findViewById<LinearLayout>(R.id.lyIntervalModeSpace)
-        val lyIntervalMode = findViewById<LinearLayout>(R.id.lyIntervalMode)
-        val lyChallengesSpace = findViewById<LinearLayout>(R.id.lyChallengesSpace)
-        val lyChallenges = findViewById<LinearLayout>(R.id.lyChallenges)
-        val lySettingsVolumesSpace = findViewById<LinearLayout>(R.id.lySettingsVolumesSpace)
-        val lySettingsVolumes = findViewById<LinearLayout>(R.id.lySettingsVolumes)
-        var lySoftTrack = findViewById<LinearLayout>(R.id.lySoftTrack)
-        var lySoftVolume = findViewById<LinearLayout>(R.id.lySoftVolume)
-        setHeightLinearLayout(lyMap, 0)
-        setHeightLinearLayout(lyIntervalModeSpace,0)
-        setHeightLinearLayout(lyChallengesSpace,0)
-        setHeightLinearLayout(lySettingsVolumesSpace,0)
-        setHeightLinearLayout(lySoftTrack,0)
-        setHeightLinearLayout(lySoftVolume,0)
-
-        lyFragmentMap.translationY = -300f
-        lyIntervalMode.translationY = -300f
-        lyChallenges.translationY = -300f
-        lySettingsVolumes.translationY = -300f
-        lyFragmentMap.translationY = -300f
-
-        swIntervalMode = findViewById(R.id.swIntervalMode)
-        swChallenges = findViewById(R.id.swChallenges)
-        swVolumes = findViewById(R.id.swVolumes)
     }
     private fun initNavigationView() {
         var navigationView: NavigationView = findViewById(R.id.nav_view)
--- app/src/main/java/com/matiasdev/wildrunning/Utility.kt
+++ app/src/main/java/com/matiasdev/wildrunning/Utility.kt
@@ -3,8 +3,36 @@ package com.matiasdev.wildrunning
 import android.animation.ObjectAnimator
 import android.view.View
 import android.widget.LinearLayout
+import java.util.concurrent.TimeUnit
 
 object Utility {
+
+    fun getFormattedStopWatch(ms: Long): String{
+        var milliseconds = ms
+        val hours = TimeUnit.MILLISECONDS.toHours(milliseconds)
+        milliseconds -= TimeUnit.HOURS.toMillis(hours)
+        val minutes = TimeUnit.MILLISECONDS.toMinutes(milliseconds)
+        milliseconds -= TimeUnit.MINUTES.toMillis(minutes)
+        val seconds = TimeUnit.MILLISECONDS.toSeconds(milliseconds)
+
+        return "${if (hours < 10) "0" else ""}$hours:" +
+                "${if (minutes < 10) "0" else ""}$minutes:" +
+                "${if (seconds < 10) "0" else ""}$seconds"
+    }
+
+    fun getSecFromWatch (watch: String): Int{
+
+        var secs = 0
+        var w: String = watch
+        if (w.length == 5) w= "00:" + w
+
+        // 00:00:00
+        secs += w.subSequence(0,2).toString().toInt() * 3600
+        secs += w.subSequence(3,5).toString().toInt() * 60
+        secs += w.subSequence(6,8).toString().toInt()
+
+        return secs
+    }
 
     fun setHeightLinearLayout(ly: LinearLayout, value: Int){
         val params: LinearLayout.LayoutParams = ly.layoutParams as LinearLayout.LayoutParams
